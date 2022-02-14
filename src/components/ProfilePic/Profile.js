397f3957444e01a518dac930d01acc51098f9a16
Parthudi <65971741+Parthudi@users.noreply.github.com>
2022-02-14 22:30:08 +0530

    adding changes

--- src/components/ProfilePic/Profile.js
+++ src/components/ProfilePic/Profile.js
@@ -4,7 +4,7 @@ import { Img, CenterDiv, HeaderThree } from './ProfilePicStyles';
 const ProfilePics = () => (
   <>
         <CenterDiv>
-            <Img src="/images/Me.PNG"  alt="Picture of the author" />
+            <Img src="/Me.PNG"  alt="Picture of the author" />
             <HeaderThree title> PARTH PARMAR</HeaderThree>
         </CenterDiv>
   </>
