694970ac98b3eabfdcf550236644dd5a33397245
Parthudi <65971741+Parthudi@users.noreply.github.com>
2022-02-14 23:19:13 +0530

    adding perfect images

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,16 +2,8 @@ import React from 'react';
 import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import {Paper} from "@mui/icons-material";
 
 const Projects = () => (
-  <div>
   <Section nopadding id="projects">
     <SectionDivider />
     <SectionTitle main>Projects</SectionTitle>
@@ -42,45 +34,6 @@ const Projects = () => (
       })}
     </GridContainer>
   </Section>
-
-  <span>
-    {projects.map((p, i) => {
-      return (
-        <Card sx={{ maxWidth: 545 }}>
-          <CardMedia
-            component="img"
-            width="100%"
-            height="100%"
-            image={p.photo}
-            alt="project image"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              {p.title}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Stack
-            </Typography>
-
-            <Typography variant="body2" color="text.secondary">
-              {p.description}
-            </Typography>
-
-            <Typography variant="body2" color="text.secondary">
-              {p.tags.map((t, i) => {
-                    return <Tag key={i}>{t}</Tag>;
-                  })}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button> <a href={p.visit} target="_blank"> Code </a></Button>
-            <Button> <a href={p.source} target="_blank"> Source </a></Button>
-          </CardActions>
-      </Card>
-    )})}
-    </span>
   );
-  </div>
-);
 
 export default Projects;
